6abd8d3aa1004d510c322c2a08c8ad3a76f8db2e
mirek kulha <miroslav.kulha@embedit.cz>
2015-07-02 13:42:20 +0200

    Aurelia updated.

--- config.js
+++ config.js
@@ -16,30 +16,36 @@ System.config({
 System.config({
   "map": {
     "Intl.js": "github:andyearnshaw/Intl.js@0.1.4",
-    "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.4.0",
-    "aurelia-loader-default": "github:aurelia/loader-default@0.7.0",
-    "babel": "npm:babel-core@5.4.4",
-    "babel-runtime": "npm:babel-runtime@5.4.4",
-    "core-js": "npm:core-js@0.9.11",
+    "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.6.0",
+    "aurelia-loader-default": "github:aurelia/loader-default@0.9.0",
+    "babel": "npm:babel-core@5.6.15",
+    "babel-runtime": "npm:babel-runtime@5.6.15",
+    "core-js": "npm:core-js@0.9.18",
     "i18next": "github:i18next/i18next@1.9.0",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "github:aurelia/loader-default@0.7.0": {
-      "aurelia-loader": "github:aurelia/loader@0.6.0",
-      "aurelia-metadata": "github:aurelia/metadata@0.5.0"
+    "github:aurelia/event-aggregator@0.6.0": {
+      "aurelia-logging": "github:aurelia/logging@0.6.0"
     },
-    "github:aurelia/loader@0.6.0": {
+    "github:aurelia/loader-default@0.9.0": {
+      "aurelia-loader": "github:aurelia/loader@0.8.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.7.0"
+    },
+    "github:aurelia/loader@0.8.0": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
-      "aurelia-path": "github:aurelia/path@0.6.1",
-      "core-js": "npm:core-js@0.9.11",
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.1"
+      "aurelia-path": "github:aurelia/path@0.8.0",
+      "core-js": "npm:core-js@0.9.18",
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.3"
     },
-    "github:aurelia/metadata@0.5.0": {
-      "core-js": "npm:core-js@0.9.11"
+    "github:aurelia/metadata@0.7.0": {
+      "core-js": "npm:core-js@0.9.18"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:core-js@0.9.11": {
+    "npm:babel-runtime@5.6.15": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
